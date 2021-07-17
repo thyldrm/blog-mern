5574087441a89606dafcac399e9b74a3ea5f7dfb
Taha Yıldırım <thyldrm23@gmail.com>
2021-07-17 13:42:07 +0300

    Create sidebar & posts

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import Header from '../../header/Header'
+import Posts from '../../posts/Posts'
+import Sidebar from '../../sidebar/Sidebar'
 import "./home.css"
 
 export default function Home() {
     return (
+        <>
+        <Header/>
         <div className="home">
-            <Header/>
-            homepage
+            <Posts/>
+            <Sidebar/>
         </div>
+        </>
     )
 }
