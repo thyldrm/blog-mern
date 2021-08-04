c8bdaf1003638fac74ed4f23cbb10cd51218fe04
Taha Yıldırım <thyldrm23@gmail.com>
2021-08-04 13:10:13 +0300

    Create login & register

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import TopBar from "./components/topbar/TopBar";
 import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
 import Settings from "./pages/settings/Setting";
 import Single from "./pages/single/Single";
 import Write from "./pages/write/Write";
@@ -8,7 +10,7 @@ function App() {
   return (
     <>
       <TopBar/>
-      <Settings/>
+      <Register/>
     </>
   );
 }
