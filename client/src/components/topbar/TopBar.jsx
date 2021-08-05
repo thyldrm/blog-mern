670c631a9f201b20acc005d6ac262418cbac8931
Taha Yıldırım <thyldrm23@gmail.com>
2021-08-05 14:22:36 +0300

    Moved files

--- client/src/components/topbar/TopBar.jsx
+++ client/src/components/topbar/TopBar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import "./topbar.css"
 
 export default function TopBar() {
-    const user = false;
+    const user = true;
     return (
         <div className="top">
             <div className="topLeft">
