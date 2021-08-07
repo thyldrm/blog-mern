270a03b989ba6519f8f84886688615d55612a38c
Taha Yıldırım <thyldrm23@gmail.com>
2021-08-07 16:15:28 +0300

    Create posts delete&get process

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -39,11 +39,36 @@ router.put("/:id", async (req,res)=>{
 });
 
 
-
 //--DELETE POST
+
+router.delete("/:id", async (req,res)=>{
+    try {
+        const post = await Post.findById(req.params.id);
+        if(post.username === req.body.username){
+            try {
+            await post.delete();
+            res.status(200).json("Gönderi silindi.");
+            }catch(err){
+                res.status(500).json(err)
+            }
+        } else{
+            res.status(404).json("Sadece gönderinizi silebilirsiniz.")
+        }
+    } catch (err) {
+        res.status(500).json(err)
+    }
+});
 
 //--GET POST
 
+router.get("/:id", async (req,res)=>{
+    try{
+        const post = await Post.findById(req.params.id)
+        res.status(200).json(post);
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
 
 
 
