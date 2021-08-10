f576b279e59876b731ed2e86b3f68501df98a9b9
Taha Yıldırım <thyldrm23@gmail.com>
2021-08-10 16:52:24 +0300

    Add fetch single post

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -1,4 +1,5 @@
 import "./post.css"
+import {Link} from "react-router-dom";
 
 
 export default function Post({post}) {
@@ -17,9 +18,11 @@ export default function Post({post}) {
                     </span>
                     ))}
                 </div>
-                <span className="postTitle">
-                    {post.title}
-                </span>
+                <Link to={`/post/${post._id}`} className="link">
+                    <span className="postTitle">
+                      {post.title}
+                    </span>
+                </Link>
                 <hr />
                 <span className="postDate">
                     {new Date(post.createdAt).toDateString()}
--- client/src/components/singlePost/SinglePost.jsx
+++ client/src/components/singlePost/SinglePost.jsx
@@ -1,24 +1,41 @@
 import "./singlepost.css"
+import { useState,useEffect } from 'react'
+import { useLocation } from 'react-router'
+import axios from "axios"
 
 export default function SinglePost() {
+    const location = useLocation();
+    const path = location.pathname.split("/")[2];
+    const [post, setPost] = useState({})
+
+    useEffect(() => {
+        const getPost = async () => {
+            const res = await axios.get("http://localhost:8080/api/posts/" + path);
+            setPost(res.data)
+        };
+        getPost()
+    }, [path])
+
     return (
         <div className="singlePost">
             <div className="singlePostWrapper">
-                <img src="https://picsum.photos/id/1/1200/800" alt="" className="singlePostImg"
+            {post.photo && 
+                <img src={post.photo} alt="" className="singlePostImg"
                 />
-                <h1 className="singlePostTitle">Lorem ipsum dolor sit amet
+            }
+                <h1 className="singlePostTitle">{post.title}
                     <div className="singlePostEdit">
                         <i className="singlePostIcon far fa-edit"></i>
                         <i className="singlePostIcon far fa-trash-alt"></i>
                     </div>
                 </h1>
                 <div className="singlePostInfo">
-                    <span className="singlePostAuthor">Author: <b>TT</b>
+                    <span className="singlePostAuthor">Author: <b>{post.username}</b>
                     </span>
-                    <span className="singlePostDate">1 hour ago
+                    <span className="singlePostDate">{post.createdAt}
                     </span>
                 </div>
-                <p className="singlePostDesc">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellendus nobis aliquid quibusdam aliquam, enim nisi nemo repudiandae asperiores! Pariatur minus saepe facere odio cumque repellendus animi iste, in adipisci sequi, excepturi est nesciunt ad voluptatum atque corporis. Nam at quisquam incidunt vitae, delectus voluptatibus. Quia velit dolorum laboriosam, architecto distinctio nemo dolor excepturi nobis officiis culpa temporibus odio. Vel a doloribus natus. Maxime dolorum fugit magnam? Autem nam minus suscipit obcaecati exercitationem rerum voluptatem rem odio praesentium earum sit eaque accusamus nihil repellendus architecto ducimus, commodi dolores, sapiente velit maiores expedita porro. Similique ut sapiente laboriosam exercitationem itaque eaque. Inventore doloribus doloremque nesciunt officiis excepturi repudiandae voluptate iure magni, quae sit ipsum, mollitia illo odio recusandae voluptatem, laborum soluta! Temporibus maxime distinctio laudantium illum tenetur dolore neque. Ullam labore minus blanditiis quia architecto eligendi unde, iusto animi expedita velit magni. Quos adipisci accusantium fugiat minus, sit nostrum facere officia vero.</p>
+                <p className="singlePostDesc">{post.desc}</p>
             </div>
         </div>
     )
