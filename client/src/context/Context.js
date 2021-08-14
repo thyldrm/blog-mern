f65b0bcafefc713fc72f45c413a74f4b8bc3af86
Taha Yıldırım <thyldrm23@gmail.com>
2021-08-14 15:50:18 +0300

    Add context api

---
+++ client/src/context/Context.js
@@ -0,0 +1,28 @@
+import { createContext, useReducer } from "react";
+import Reducer from "./Reducer";
+
+
+const INITIAL_STATE = {
+    user: null,
+    isFetching: false,
+    error: false,
+};
+
+export const Context = createContext(INITIAL_STATE);
+
+export const ContextProvider = ({children}) => {
+    const [state, dispatch] = useReducer(Reducer, INITIAL_STATE);
+
+    return(
+        <ContextProvider
+        value={{
+            user:state.user,
+            isFetching:state.isFetching,
+            error:state.error,
+            dispatch,
+        }}
+        >
+            {children}
+        </ContextProvider>
+    )
+}
--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {ContextProvider} from "./context/Context"
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ContextProvider>
+      <App />
+    </ContextProvider>
+    
   </React.StrictMode>,
   document.getElementById('root')
 );
